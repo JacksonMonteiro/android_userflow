02b71cb7833aaf991fb2eeedf848a5ab00dbb09e
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2024-01-11 12:11:06 -0300

    Criação da entidade usuário

---
+++ app/src/main/java/com/gfxconsultoria/users/model/Usuario.java
@@ -0,0 +1,129 @@
+package com.gfxconsultoria.users.model;
+
+/*
+Created By Jackson Monteiro on 11/01/2024
+*/
+
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "usuarios")
+public class Usuario {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String nome;
+    private String username;
+    private String password;
+    private String foto;
+    private String endereco;
+    private String email;
+    private Date dataNascimento;
+    private String sexo;
+    private String tipo;
+    private String cpfCnpj;
+
+    public Usuario(String nome, String username, String password, String foto, String endereco, String email, Date dataNascimento, String sexo, String tipo, String cpfCnpj) {
+        this.nome = nome;
+        this.username = username;
+        this.password = password;
+        this.foto = foto;
+        this.endereco = endereco;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.tipo = tipo;
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+}
