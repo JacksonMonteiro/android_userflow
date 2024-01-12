367f2baff7a156bc21f69f051fa9910779242386
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2024-01-11 21:04:50 -0300

    Criação do item da recyclerView de usuários, criação do adapter e da recyclerView e exibição na MainActivity

--- app/src/main/java/space/jacksonmonteiro/users/MainActivity.java
+++ app/src/main/java/space/jacksonmonteiro/users/MainActivity.java
@@ -1,11 +1,13 @@
 package space.jacksonmonteiro.users;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import space.jacksonmonteiro.users.ui.user.adapter.UserAdapter;
 import space.jacksonmonteiro.users.viewmodels.UserViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,11 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final UserAdapter adapter = new UserAdapter();
+
+        RecyclerView recyclerView = findViewById(R.id.usersRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         userViewModel.getAllUsers().observe(
                 this,
                 users -> {
-                    Toast.makeText(this, "All users loaded successful", Toast.LENGTH_LONG).show();
+                    adapter.setUsers(users);
                 }
         );
     }
---
+++ app/src/main/java/space/jacksonmonteiro/users/ui/user/adapter/UserAdapter.java
@@ -0,0 +1,67 @@
+package space.jacksonmonteiro.users.ui.user.adapter;
+/*
+Created By Jackson Monteiro on 11/01/2024
+Copyright (c) 2024 GFX Consultoria
+*/
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import space.jacksonmonteiro.users.R;
+import space.jacksonmonteiro.users.models.User;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
+    private List<User> users = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public UserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_item, parent, false);
+        return new UserHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserHolder holder, int position) {
+        User currentUser = users.get(position);
+
+        holder.tvName.setText(currentUser.getNome());
+        holder.tvUsername.setText(currentUser.getUsername());
+        holder.tvEmail.setText(currentUser.getEmail());
+        holder.tvCpfCnpj.setText(currentUser.getCpfCnpj());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+        notifyDataSetChanged();
+    }
+
+    class UserHolder extends RecyclerView.ViewHolder {
+        private TextView tvName;
+        private TextView tvUsername;
+        private TextView tvEmail;
+        private TextView tvCpfCnpj;
+
+        public UserHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.nameValue);
+            tvUsername = itemView.findViewById(R.id.usernameValue);
+            tvEmail = itemView.findViewById(R.id.emailValue);
+            tvCpfCnpj = itemView.findViewById(R.id.cpfCnpjValue);
+        }
+    }
+}
