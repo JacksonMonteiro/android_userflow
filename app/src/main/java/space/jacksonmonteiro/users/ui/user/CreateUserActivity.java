6ee992d1a662a6dd9f43a49d458e7b7db32aebbe
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2024-01-12 18:22:05 -0300

    Início do desenvolvimento das validações para criar usuário

--- app/src/main/java/space/jacksonmonteiro/users/ui/user/CreateUserActivity.java
+++ app/src/main/java/space/jacksonmonteiro/users/ui/user/CreateUserActivity.java
@@ -3,7 +3,9 @@ package space.jacksonmonteiro.users.ui.user;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,19 +14,32 @@ import space.jacksonmonteiro.users.R;
 public class CreateUserActivity extends AppCompatActivity {
     private Spinner spinnerSexo, spinnerTipo;
     private Button btnChooseImage, btnCreateUser, btnGoBack;
+    private EditText etNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
 
-        btnGoBack = findViewById(R.id.btnGoBack);
+        // EditTexts
+        etNome = findViewById(R.id.etNome);
 
+        // Buttons
+        btnGoBack = findViewById(R.id.btnGoBack);
+        btnCreateUser = findViewById(R.id.buttonAddUser);
+
+        // Spinners
         spinnerSexo = findViewById(R.id.spinnerSexo);
         spinnerTipo = findViewById(R.id.spinnerTipo);
 
         setupSpinner(R.array.sexo, spinnerSexo);
         setupSpinner(R.array.tipo, spinnerTipo);
+
+        btnCreateUser.setOnClickListener(v -> {
+            if (validateForm()) {
+                Toast.makeText(this, "Todos os campos validados com sucesso", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         btnGoBack.setOnClickListener(v -> {
             finish();
@@ -40,5 +55,16 @@ public class CreateUserActivity extends AppCompatActivity {
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+    }
+
+    public boolean validateForm() {
+        String nome = etNome.getText().toString().trim();
+
+        if (nome.length() < 30) {
+            etNome.setError("Seu nome deve ter no mínimo 30 caracteres");
+            return false;
+        }
+
+        return true;
     }
 }
